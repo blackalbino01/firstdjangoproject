4f60f94c1718e89884afe5bb0abab25fe24b6dbb
Hussaini Auwal <hauwal4969@gmail.com>
2020-05-02 05:52:44 +0100

    successfully fixed

---
+++ mysite/mysite/urls.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from django.urls import path,include
+
+
+
+urlpatterns = [
+    path('blog/',include('blog.urls')),
+    path('admin/', admin.site.urls),
+    path('projects',include('projects.urls')),
+]
